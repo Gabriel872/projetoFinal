b66d290a434fe4ea8ca206cbc78da1b75cd00634
Bruna <bruna.jansen.bjs@gmail.com>
2022-07-19 09:07:31 -0300

    api

---
+++ DapperTrabalhoFinal/DapperTrabalhoFinal/Models/Classes.cs
@@ -0,0 +1,16 @@
+﻿namespace DapperTrabalhoFinal.Models
+{
+    public class Classes
+    {
+
+        private int id_class;
+        private string class_title;
+        private string class_video;
+        private int class_complete;
+
+        public int Id_class { get => id_class; set => id_class = value; }
+        public string Class_title { get => class_title; set => class_title = value; }
+        public string Class_video { get => class_video; set => class_video = value; }
+        public int Class_complete { get => class_complete; set => class_complete = value; }
+    }
+}
---
+++ DapperTrabalhoFinal/DapperTrabalhoFinal/Models/Interests.cs
@@ -0,0 +1,14 @@
+﻿namespace DapperTrabalhoFinal.Models
+{
+    public class Interests
+    {
+
+        private int id_interests;
+        private int id_categorie;
+        private int id_user;
+
+        public int Id_interests { get => id_interests; set => id_interests = value; }
+        public int Id_categorie { get => id_categorie; set => id_categorie = value; }
+        public int Id_user { get => id_user; set => id_user = value; }
+    }
+}
