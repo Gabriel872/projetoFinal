7ade78ca0390601568b63042458697d5e3a597ea
Gabriel Bublitz <gabriel.bublitz@hotmail.com>
2022-07-21 17:28:37 -0300

    Alterações home page

--- front_end/src/views/HomeView.vue
+++ front_end/src/views/HomeView.vue
@@ -59,19 +59,28 @@
     </section>
     <section class="third">
       <div class="container">
-        <div class="row">
+        <div class="row flex" style="justify-content:space-evenly ;">
           <div class="col-4">
-            <div class="promotion-card">
-              Aprenda as habilidades mais procuradas com mais de 185.000 cursos em vídeo
+            <div class="promotion-card painels" style="justify-content: center;">
+              <div class="ps-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                  class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path
+                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+              </div>
+              <div class="ms-4">
+                Aprenda as habilidades mais procuradas com mais de 185.000 cursos em vídeo
+              </div>
             </div>
           </div>
           <div class="col-4">
-            <div class="promotion-card">
+            <div class="promotion-card painels">
               Escolha cursos lecionados por especialistas reais
             </div>
           </div>
           <div class="col-4">
-            <div class="promotion-card">
+            <div class="promotion-card painels">
               Estude no seu ritmo, com acesso vitalício no dispositivo móvel e no computador
             </div>
           </div>
@@ -152,6 +161,11 @@ section {
   margin: 0px auto 10px auto;
 }
 
+.painels {
+  background-color: lightgray;
+  border-radius: 20px;
+}
+
 .carousel__prev {
   background-color: black !important;
 }
@@ -165,7 +179,6 @@ section {
 }
 
 .third {
-  background-color: lightgrey;
   padding: 0px;
 }
 
